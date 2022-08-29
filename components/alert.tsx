b26aa56c1310b1b51ef46ee24fa03a5b67c55e3c
msbsylmz <musabsoylemez@hotmail.com>
2022-08-29 16:38:43 +0300

    Added custom alert

---
+++ components/alert.tsx
@@ -0,0 +1,12 @@
+function Alert() {
+	return (
+		<div className="h-screen w-full top-0 left-0 fixed flex justify-center items-center bg-gray opacity-50 z-50">
+			<div className="w-96 h-72 bg-purple ">
+				<h6></h6>
+				<p>Message is here</p>
+			</div>
+		</div>
+	);
+}
+
+export default Alert;
